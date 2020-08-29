eee12a999d27c467706a2feedb5b73167113835e
msaaddev <mrsaadirfan@gmail.com>
2020-08-29 07:06:12 +0500

    👌 IMPROVE: error handling

--- utils/cli.js
+++ utils/cli.js
@@ -3,15 +3,17 @@ const jsonFile = require('jsonfile');
 const user = require('./user');
 const display = require('./display');
 const ora = require('ora');
+const chalk = require('chalk');
+const symbols = require('log-symbols');
 
 module.exports = async () => {
     let followers = [];
     let page = 1;
     const username = await user();
-    console.log('');
+    const spinner = ora('Fetching GitHub followers');
+    spinner.start();
+
     try {
-        const spinner = ora('Fetching GitHub followers');
-        spinner.start();
         while (true) {
             const options = {
                 method: 'GET',
@@ -32,6 +34,19 @@ module.exports = async () => {
 
         jsonFile.writeFile('./data/follower.json', followers, err => {});
     } catch (error) {
-        console.log(error);
+        spinner.fail();
+        if (error.response.status === 404) {
+            console.log('');
+            console.log(symbols.error, chalk.hex('#FF0000').inverse(' ERROR '), 'Invalid username');
+            const temp = [];
+            jsonFile.writeFile('./data/user.json', temp, err => {});
+        } else {
+            console.log('');
+            console.log(
+                symbols.warning,
+                chalk.hex('#FF0000').inverse(' WARNING '),
+                'Try again later!'
+            );
+        }
     }
 };
