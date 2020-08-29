7d7948028f2dac91dbb625cac03dd9b38797ef3f
msaaddev <mrsaadirfan@gmail.com>
2020-08-29 15:18:12 +0500

    🐛 FIX: module export

--- utils/display.js
+++ utils/display.js
@@ -1,33 +1,37 @@
 const pwd = process.cwd();
-const followerList = require(`${pwd}/follower`);
 const table = require('./table');
 const chalk = require('chalk');
 const symbols = require('log-symbols');
 const unfollow = require('./unfollow');
 
-module.exports = followers => {
-    if (followerList.length !== 0) {
-        const list = unfollow(followers);
-        if (list.length !== 0) {
-            console.log(
-                symbols.warning,
-                chalk.hex('#FF0000').inverse(' WARNING '),
-                'Following folk/s have unfollowed you recently.'
-            );
-            table(list);
-        } else {
-            console.log(
-                symbols.info,
-                chalk.hex('#5E3AFF').inverse(' INFO '),
-                'Yeay! No one has unfollowed you recently.'
-            );
+try {
+    const followerList = require(`${pwd}/follower`);
+    module.exports = followers => {
+        if (followerList.length !== 0) {
+            const list = unfollow(followers);
+            if (list.length !== 0) {
+                console.log(
+                    symbols.warning,
+                    chalk.hex('#FF0000').inverse(' WARNING '),
+                    'Following folk/s have unfollowed you recently.'
+                );
+                table(list);
+            } else {
+                console.log(
+                    symbols.info,
+                    chalk.hex('#5E3AFF').inverse(' INFO '),
+                    'Yeay! No one has unfollowed you recently.'
+                );
+            }
         }
-    } else {
+    };
+} catch (error) {
+    module.exports = followers => {
         console.log('');
         console.log(
             symbols.success,
             chalk.hex('#DF661E').inverse(' INFO '),
             'Your username & followers list has been saved for later use.'
         );
-    }
-};
+    };
+}
--- utils/unfollow.js
+++ utils/unfollow.js
@@ -1,16 +1,18 @@
 const pwd = process.cwd();
-const list = require(`${pwd}/follower.json`);
+try {
+    const list = require(`${pwd}/follower.json`);
 
-module.exports = followers => {
-    if (list.length === 0) return;
+    module.exports = followers => {
+        if (list.length === 0) return;
 
-    followers.forEach(n =>
-        list.forEach(m => {
-            if (n === m) {
-                const index = list.indexOf(n);
-                list.splice(index, 1);
-            }
-        })
-    );
-    return list;
-};
+        followers.forEach(n =>
+            list.forEach(m => {
+                if (n === m) {
+                    const index = list.indexOf(n);
+                    list.splice(index, 1);
+                }
+            })
+        );
+        return list;
+    };
+} catch (error) {}
