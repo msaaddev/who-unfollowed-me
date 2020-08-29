f1ca1ea01660e7fd27edcd04fe9e5d7668acd1c9
msaaddev <mrsaadirfan@gmail.com>
2020-08-29 06:20:49 +0500

    📦 NEW: username information

---
+++ utils/user.js
@@ -0,0 +1,12 @@
+const io = require('console-read-write');
+const jsonFile = require('jsonfile');
+const user = require('../data/user');
+
+module.exports = async () => {
+    if (user.length === 0) {
+        io.write('Enter your GitHub username: ');
+        const username = await io.read();
+        jsonFile.writeFile('./data/user.json', username, err => {});
+        return username;
+    } else return user;
+};
